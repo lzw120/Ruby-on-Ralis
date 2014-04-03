41ea58fba748b627b1a6c416a1c455b9c7f00f87
ZewenLi <zewenli@ZewenLis-MacBook-Pro.local>
2014-04-03 13:39:18 -0700

    add filling-in-layout

---
+++ demo_app_bk/config/initializers/secret_token.rb
@@ -0,0 +1,25 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+# DemoApp::Application.config.secret_key_base = '7b9250d375970df22e7935b852dec2db23e6069b21cbfe4038f638cb3155c4a6ae4a5ba67712bd28dd4fd2e82435a1525999571a34b8af79821f6b0007d87066'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    File.read(token_file).chomp
+  else
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+DemoApp::Application.config.secret_key_base = secure_token
