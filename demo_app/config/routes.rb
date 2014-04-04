ab2cf9c9e0be1a9b9412d612c6a648326e77bf4b
lzw120 <seven.lea@gmail.com>
2014-04-04 10:43:01 -0700

    finished first part

--- demo_app/config/routes.rb
+++ demo_app/config/routes.rb
@@ -1,7 +1,10 @@
 DemoApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+# get "static_pages/home"
+#  match '/', to: 'static_pages#home', via: 'get'
+  root 'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   resources :microposts
 
   resources :users
@@ -10,7 +13,7 @@ DemoApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
