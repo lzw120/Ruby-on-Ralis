396e9d734b26cadb16a3f7d4d572b62a5638c10e
lzw120 <seven.lea@gmail.com>
2014-04-04 10:55:59 -0700

    finish layout routes

--- demo_app/config/routes.rb
+++ demo_app/config/routes.rb
@@ -1,7 +1,9 @@
 DemoApp::Application.routes.draw do
+  get "users/new"
 # get "static_pages/home"
 #  match '/', to: 'static_pages#home', via: 'get'
   root 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
