30c04d1824c98ca50c5cb1ff5eee39a9660ab484
lzw120 <seven.lea@gmail.com>
2014-04-25 10:01:06 -0700

    modeling users

---
+++ modeling-users/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ModelingUsers::Application.config.secret_key_base = '0690e4562d6873d7618f54d7f002303f138b981c1655dd8877701668164b569510c340348eac44451ec282fd187530100ab3b961217bb0d2d1072cd15e33065e'
---
+++ modeling-users/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ModelingUsers::Application.config.session_store :cookie_store, key: '_modeling-users_session'
